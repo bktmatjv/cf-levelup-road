0b5ccf4d7eeaacb2f4bd94d81b3d27ea53b2e152
Matias Del Castillo <168279361+bktmatjv@users.noreply.github.com>
2025-07-07 20:03:36 -0500

    Se agregó competitive.cpp

---
+++ templates/competitive.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define DBG(x) cerr << #x << " = " << (x) << endl
+
+#define fori(_, n) for(int i = _; i<n; i++)
+#define ll long long
+#define endl "\n"
+
+int c= 0;
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1; //cin>>t;
+    while(t--) solve();
+
+
+    return 0;
+}
