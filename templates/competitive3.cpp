49e69ea7757da735ab23ebc7115b743effec18b3
Matias Del Castillo <168279361+bktmatjv@users.noreply.github.com>
2025-09-24 11:16:24 -0500

    compActual

---
+++ templates/competitive3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using  vll = vector<long long>; 
+using vi = vector<int>;
+using mii = map<int,int>;
+
+#define endl "\n"
+#define sz(s) s.size()
+#define fori(_, n) for(int i =_; i<n; i++)
+#define forj(_, n) for(int j =_; j<n; j++)
+#define all(arr) arr.begin(), arr.end()
+#define rall(arr) arr.rbegin(), arr.rend()
+#define pb push_back
+#define fi first
+#define se second
+
+#define dbg(x) cerr << #x << " = " << (x) << endl
+#define dbgv(v) cerr << #v << " = ["; for (auto _ : v) cerr << _ << " "; cerr << "]" << endl;
+#define dbgvp(v) cerr << #v << " = ["; for (auto [_f, _s] : v) cerr << "(" << _f << ", " << _s << ") "; cerr << "]" << endl;
+
+void solve() {
+    
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t=1; cin>>t;
+    while(t--) solve();
+}
